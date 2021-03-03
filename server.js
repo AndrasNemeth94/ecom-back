6b3747010a65d96d4b9bb7c9d50675e057eaad0d
AndrasNemeth94 <nemethand94@gmail.com>
2021-03-03 10:14:21 +0100

    server config

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const port = 3000;
+const host = "0.0.0.0";
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", api);
@@ -13,6 +14,6 @@ app.get("/", (req, res) => {
   res.json("server is up and running");
 });
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log("Server is listening on: " + port);
 });
