52a7a097e5145f34f8e95d3a695ea5cec36dcbfd
AndrasNemeth94 <nemethand94@gmail.com>
2021-03-03 14:35:51 +0100

    heroku based backend

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const api = require("./routes/api");
 const bodyParser = require("body-parser");
 const app = express();
 
-const port = process.env.PORT || 3000;
+/* const port = process.env.PORT || 3000; */
+//localhost:
+const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", api);
